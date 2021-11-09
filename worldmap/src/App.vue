4dbe572393f1492fc17c045cbd1d04fe8cae6d88
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-11-08 23:30:51 -0500

    tested map click lat/long

--- worldmap/src/App.vue
+++ worldmap/src/App.vue
@@ -1,92 +1,69 @@
 <template>
-
   <div id="app">
-
-    <div class="Character">   
-      <img class="Character_shadow pixelart" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png" alt="Shadow" /> 
-      <img class="Character_spritesheet pixelart face-down" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png" alt="Character" /> 
-    </div>
-
-    <div class="Character">   
-      <img class="Character_shadow pixelart" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png" alt="Shadow" /> 
-      <img class="Character_spritesheet pixelart face-right" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png" alt="Character" /> 
-    </div>
-
-        <div class="Character">   
-      <img class="Character_shadow pixelart" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png" alt="Shadow" /> 
-      <img class="Character_spritesheet pixelart face-left" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png" alt="Character" /> 
-    </div>
-
-        <div class="Character">   
-      <img class="Character_shadow pixelart" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png" alt="Shadow" /> 
-      <img class="Character_spritesheet pixelart face-up" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png" alt="Character" /> 
-    </div>
-
+     <div class="map-container">
+        <img class="map" src="@/assets/us-map.jpg" alt="" @click="mapClick($event)">
+     </div>
   </div>
-
 </template>
+
+<script>
+
+export default {
+   data() {
+      return {
+         latTop: 52,
+         latBot: 22,
+         longLeft: 127,
+         longRight: 62,
+      }
+   },
+   methods: {
+      mapClick(event) {
+         const elRect = event.currentTarget.getBoundingClientRect();
+         const pixelsX = event.clientX - elRect.left;
+         const pixelsY = event.clientY - elRect.top;
+
+         const percentX = pixelsX / elRect.width;
+         const percentY = pixelsY / elRect.height;
+
+         const latRange = this.latBot - this.latTop;
+         const longRange = this.longRight - this.longLeft;
+
+         const latitude = this.latTop + (percentX * latRange);
+         const longitude = this.longLeft + (percentY * longRange);
+
+         console.log(latitude,longitude);
+      }
+   },
+   mounted () {},
+};
+
+</script>
+
 
 <style lang="scss">
 
-// #app {
-//   font-family: Avenir, Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// }
-
-:root {
-   --pixel-size: 6;
+#app {
+   width: 100%;
+   height: 100%;
 }
 
 body {
    background: #b5fcf6;
+   margin: 0 !important;
+   padding: 0 !important;
 }
 
-.Character {
-   width: calc(32px * var(--pixel-size));
-   height: calc(32px * var(--pixel-size));
-   overflow: hidden;
-   position: relative;
-   margin: 4em auto;
+.map-container {
+   width: 100%;
+   height: 100%;
+   display: flex;
+   align-items: center;
+   justify-content: space-evenly;
 }
 
-.Character_spritesheet {
-   animation: moveSpritesheet 1s steps(4) infinite;
-   width: calc(128px * var(--pixel-size));
-   position: absolute;
-   
+.map {
+   width: 60rem;
 }
-.Character_shadow {
-   position: absolute;
-   width: calc(32px * var(--pixel-size));
-   height: calc(32px * var(--pixel-size));
-}
-
-.pixelart {
-   image-rendering: pixelated;
-}
-
-.face-right {
-   top: calc(-32px * var(--pixel-size));
-}
-.face-up {
-   top: calc(-64px * var(--pixel-size));
-}
-.face-left {
-   top: calc(-96px * var(--pixel-size));
-}
-
-
-@keyframes moveSpritesheet {
-   from {
-      transform: translate3d(0px,0,0)
-   }
-   to {
-      transform: translate3d(-100%,0,0)
-   }
-}
-
 
 </style>
