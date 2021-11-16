b3cb1e96619c59ea7ade37e111ac055ba46c471b
wilsonw13 <71456315+wilsonw13@users.noreply.github.com>
2021-11-16 12:28:44 -0500

    finally got the console image

--- worldmap/src/services/EventService.js
+++ worldmap/src/services/EventService.js
@@ -1,44 +1,49 @@
 const mapQuestKey = process.env.VUE_APP_MAPQUEST_API_KEY;
 
 export default {
-  /* async getMapRefactored() {
+  async getMapRefactored() {
     try {
-      const response = fetch(
+      const responsePromise = fetch(
         `http://www.mapquestapi.com/staticmap/v4/getmap?key=${mapQuestKey}&bestfit=51.8,-126.27,21.48,-64.42&size=880,540`,
         {
           method: "GET",
           mode: "cors",
-          headers: {
-            "Content-Type": "image/jpeg",
-          },
         }
       );
-      const mapImage = await response.json;
-      console.log(mapImage);
+      const response = responsePromise.then((response) =>
+        console.log(response)
+      );
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
-  }, */
-  async getMap() {
+  },
+  /* async getMap() {
     return fetch(
       `http://www.mapquestapi.com/staticmap/v4/getmap?key=${mapQuestKey}&bestfit=51.8,-126.27,21.48,-64.42&size=880,540`,
       {
         method: "GET",
         mode: "cors",
-        headers: {
-          "Content-Type": "image/jpeg",
-        },
       }
     )
       .then((response) => {
         console.log(response);
-        response.blob();
+        response.Blob();
       })
       .then((blob) => console.log(blob))
       .catch((error) => console.log(error));
+  }, */
+  async getMap() {
+    const response = await fetch(
+      `http://www.mapquestapi.com/staticmap/v4/getmap?key=${mapQuestKey}&bestfit=51.8,-126.27,21.48,-64.42&size=880,540`,
+      {
+        method: "GET",
+        mode: "cors",
+      }
+    );
+    const image = await response.blob();
+    console.log(image);
+    const imageObjectURL = URL.createObjectURL(image);
+    console.log(imageObjectURL);
   },
 };
-
-/* fetch(`http://www.mapquestapi.com/staticmap/v4/getmap?key=${process.env.VUE_APP_MAPQUEST_API_KEY}&bestfit=51.8,-126.27,21.48,-64.42&size=880,540`)
-      .then(response => console.log(JSON.stringify(response.data)))
-      .catch(error => console.log(error)); */
