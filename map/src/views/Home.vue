47db66a3a4c5eb00b034fe7f0d5b9b09a35b61e1
kentng01 <57498837+kentng01@users.noreply.github.com>
2021-11-24 08:53:48 -0500

    we're good

--- map/src/views/Home.vue
+++ map/src/views/Home.vue
@@ -1,174 +1,33 @@
 <template>
   <div class="home">
-    <div class="map-container">
-      <img class="quest-map" :src="mapURL" alt="Map" @click="getCoords($event)" />
-      <canvas class="quest-canvas" width="1120" height="672">
-        <img src />
-      </canvas>
-    </div>
-    
-    <!-- <div class="direction-container">
-      <div @click="newMapNW()">NW</div>
-      <div @click="newMapNorth()">N</div>
-      <div @click="newMapNE()">NE</div>
-      <div @click="newMapWest()">W</div>
-      <div class="div-no-color"></div>
-      <div @click="newMapEast()">E</div>
-      <div @click="newMapSW()">SW</div>
-      <div @click="newMapSouth()">S</div>
-      <div @click="newMapSE()">SE</div>
-    </div> -->
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+  <canvas width="1480" height="850">
+    <img src/>
+  </canvas>
+
   </div>
 </template>
 
+<style>
+
+body{
+  background: url("../assets/map.png") no-repeat center center fixed;
+}
+
+</style>
+
 <script>
 // @ is an alias to /src
-import APICalls from "@/APICalls.js";
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
-  components: {},
-  data() {
-    return {
-      latTop: 50,
-      latBot: 25,
-      longLeft: -125,
-      longRight: -65,
-
-      latInc: 15,
-      longInc: 36,
-
-      sourceMap: require("@/assets/map-us.jpg"),
-      currentMapURL: null,
-    };
+  name: 'Home',
+  components: {
   },
-  methods: {
-    getCoords(event) {
-      const elRect = event.currentTarget.getBoundingClientRect();
-      const pixelsX = event.clientX - elRect.left;
-      const pixelsY = event.clientY - elRect.top;
 
-      const percentX = pixelsX / elRect.width;
-      const percentY = pixelsY / elRect.height;
-
-      const latRange = this.latBot - this.latTop;
-      const longRange = this.longRight - this.longLeft;
-
-      // Calculates lat/long and rounds to 1 decimal
-      const latitude = (this.latTop + percentY * latRange).toFixed(1);
-      const longitude = (this.longLeft + percentX * longRange).toFixed(1);
-
-      console.log(`${latitude}, ${longitude}`);
-    },
-    newMapNorth() {
-      this.latTop += this.latInc;
-      this.latBot += this.latInc;
-
-      this.newMap();
-    },
-    newMapSouth() {
-      this.latTop -= this.latInc;
-      this.latBot -= this.latInc;
-
-      this.newMap();
-    },
-    newMapEast() {
-      this.longLeft += this.longInc;
-      this.longRight += this.longInc;
-
-      this.newMap();
-    },
-    newMapWest() {
-      this.longLeft -= this.longInc;
-      this.longRight -= this.longInc;
-
-      this.newMap();
-    },
-    newMapNE() {
-      this.latTop += this.latInc * 0.7;
-      this.latBot += this.latInc * 0.7;
-
-      this.longLeft += this.longInc * 0.7;
-      this.longRight += this.longInc * 0.7;
-
-      this.newMap();
-    },
-    newMapNW() {
-      this.latTop += this.latInc * 0.7;
-      this.latBot += this.latInc * 0.7;
-
-      this.longLeft -= this.longInc * 0.7;
-      this.longRight -= this.longInc * 0.7;
-
-      this.newMap();
-    },
-    newMapSE() {
-      this.latTop -= this.latInc * 0.7;
-      this.latBot -= this.latInc * 0.7;
-
-      this.longLeft += this.longInc * 0.7;
-      this.longRight += this.longInc * 0.7;
-
-      this.newMap();
-    },
-    newMapSW() {
-      this.latTop -= this.latInc * 0.7;
-      this.latBot -= this.latInc * 0.7;
-
-      this.longLeft -= this.longInc * 0.7;
-      this.longRight -= this.longInc * 0.7;
-
-      this.newMap();
-    },
-    newMap() {
-      this.coordBound();
-
-      APICalls.getMap(
-        this.latTop,
-        this.longLeft,
-        this.latBot,
-        this.longRight
-      ).then((imageURL) => (this.currentMapURL = imageURL));
-    },
-    coordBound() {
-      // If latitudeTop is ABOVE north pole, cap it to the north pole
-      if (this.latTop > 85) {
-        this.latTop = 85;
-        this.latBot = this.latTop - this.latInc;
-      }
-
-      // If latitudeBot is BELOW south pole, cap it to the south pole
-      if (this.latBot < -85) {
-        this.latBot = -85;
-        this.latTop = this.latBot + this.latInc;
-      }
-
-      // If longitude is greater than 180deg East, convert to deg West (negative degrees)
-      if (this.longLeft > 180) {
-        this.longLeft -= 360;
-      }
-      if (this.longRight > 180) {
-        this.longRight -= 360;
-      }
-
-      // If longitude is less than 180deg West (negative degrees), convert to deg East
-      if (this.longLeft < -180) {
-        this.longLeft += 360;
-      }
-      if (this.longRight < -180) {
-        this.longRight += 360;
-      }
-    },
-  },
-  computed: {
-    mapURL() {
-      // If the map is NOT null, then return it, otherwise return default map.
-      if (this.currentMapURL) return this.currentMapURL;
-      else return this.sourceMap;
-    },
-  },
-  mounted: function () {
+  mounted: function() {
     const SCALE = 3.7;
     const WIDTH = 16;
     const HEIGHT = 18.08;
@@ -182,8 +41,8 @@ export default {
     const FRAME_LIMIT = 12;
     const MOVEMENT_SPEED = 5;
 
-    let canvas = document.querySelector("canvas");
-    let ctx = canvas.getContext("2d");
+    let canvas = document.querySelector('canvas');
+    let ctx = canvas.getContext('2d');
     let keyPresses = {};
     let currentDirection = FACING_DOWN;
     let currentLoopIndex = 0;
@@ -192,36 +51,27 @@ export default {
     let positionY = 0;
     let img = new Image();
 
-    window.addEventListener("keydown", keyDownListener);
+    window.addEventListener('keydown', keyDownListener);
     function keyDownListener(event) {
-      keyPresses[event.key] = true;
+        keyPresses[event.key] = true;
     }
 
-    window.addEventListener("keyup", keyUpListener);
+    window.addEventListener('keyup', keyUpListener);
     function keyUpListener(event) {
-      keyPresses[event.key] = false;
+        keyPresses[event.key] = false;
     }
 
     function loadImage() {
-      img.src =
-        "https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png";
-      img.onload = function () {
+      img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+      img.onload = function() {
         window.requestAnimationFrame(gameLoop);
       };
     }
 
     function drawFrame(frameX, frameY, canvasX, canvasY) {
-      ctx.drawImage(
-        img,
-        frameX * WIDTH,
-        frameY * HEIGHT,
-        WIDTH,
-        HEIGHT,
-        canvasX,
-        canvasY,
-        SCALED_WIDTH,
-        SCALED_HEIGHT
-      );
+      ctx.drawImage(img,
+                    frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+                    canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
     }
 
     loadImage();
@@ -262,81 +112,21 @@ export default {
         currentLoopIndex = 0;
       }
 
-      drawFrame(
-        CYCLE_LOOP[currentLoopIndex],
-        currentDirection,
-        positionX,
-        positionY
-      );
+      drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY);
       window.requestAnimationFrame(gameLoop);
     }
 
     function moveCharacter(deltaX, deltaY, direction) {
-      if (
-        positionX + deltaX > 0 &&
-        positionX + SCALED_WIDTH + deltaX < canvas.width
-      ) {
+      if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
         positionX += deltaX;
       }
-      if (
-        positionY + deltaY > 0 &&
-        positionY + SCALED_HEIGHT + deltaY < canvas.height
-      ) {
+      if (positionY + deltaY > 0 && positionY + SCALED_HEIGHT + deltaY < canvas.height) {
         positionY += deltaY;
       }
       currentDirection = direction;
     }
-  },
-};
+  }
+
+}
+
 </script>
-
-<style>
-.home {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.map-container {
-  width: 70rem;
-  height: 42rem;
-}
-
-.quest-map {
-  width: 100%;
-  height: 100%;
-}
-
-/* .quest-canvas {
-  width: 100%;
-  height: 100%;
-} */
-
-.direction-container {
-  width: 10rem;
-  height: 10rem;
-  padding-top: 2rem;
-
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-items: center;
-}
-
-.div-no-color {
-  background-color: white !important;
-  border: 0rem !important;
-  cursor: default !important;
-}
-
-.direction-container > div {
-  width: 3rem;
-  height: 3rem;
-
-  background-color: pink;
-  cursor: pointer;
-
-  margin: auto;
-  border: 0.1rem solid;
-}
-</style>
--- worldmap/src/App.vue
+++ worldmap/src/App.vue
@@ -1,75 +1,32 @@
 <template>
   <div id="app">
-     <div class="map-container">
-        <img class="map" src="@/assets/us-map.jpg" alt="" @click="getCoords($event)">
-        <img :src="currentMapURL" alt="" @click="getCoords($event)">
-     </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import APICalls from "@/services/APICalls.js";
-
-export default {
-   data() {
-      return {
-         latTop: 51.8,
-         latBot: 21.48,
-         longLeft: -126.27,
-         longRight: -64.42,
-
-         currentMapURL: null,
-      }
-   },
-   methods: {
-      getCoords(event) {
-         const elRect = event.currentTarget.getBoundingClientRect();
-         const pixelsX = event.clientX - elRect.left;
-         const pixelsY = event.clientY - elRect.top;
-
-         const percentX = pixelsX / elRect.width;
-         const percentY = pixelsY / elRect.height;
-
-         const latRange = this.latBot - this.latTop;
-         const longRange = this.longRight - this.longLeft;
-
-         const latitude = this.latTop + (percentY * latRange);
-         const longitude = this.longLeft + (percentX * longRange);
-
-         console.log(`${latitude}, ${longitude}`);
-      },
-   },
-   created() {
-      APICalls.getMap().then(imageURL => (this.currentMapURL = imageURL));
-   },
-};
-
-</script>
-
-
-<style scope>
-
+<style lang="scss">
 #app {
-   width: 100%;
-   height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-   background: #b5fcf6;
-   margin: 0 !important;
-   padding: 0 !important;
-}
+#nav {
+  padding: 30px;
 
-.map-container {
-   width: 100%;
-   height: 100%;
-   display: flex;
-   align-items: center;
-   justify-content: space-evenly;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-.map {
-   width: 60rem;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
-
 </style>
