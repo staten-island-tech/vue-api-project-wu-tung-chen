80b41a0048ed2c0142d6a275f0b59a50c824b03b
kentng01 <57498837+kentng01@users.noreply.github.com>
2021-11-27 01:45:53 -0500

    quick fixes

--- map/src/components/Pin.vue
+++ map/src/components/Pin.vue
@@ -12,6 +12,12 @@
             <div class="header">
               <slot name="header"/>
               <button type="button" class="close" @click="closeModal()">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+              <path
+                fill="currentColor"
+                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+              ></path>
+            </svg>
               </button>
             </div>
 
@@ -170,15 +176,16 @@ h2 {
   }
 
 .close {
-    width: 30px;
+    width: 29px;
     height: 30px;
+    cursor: pointer;
   }
 
 .header {
     display: flex;
     align-items: flex-start;
-    justify-content: space-between;
-    padding: 20px 20px 10px;
+    justify-content: end;
+    padding: 7px 7px 10px;
   }
 
 .body {
